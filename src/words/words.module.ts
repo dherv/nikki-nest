a6af8cc0206be377d380c38df5725a5e1d51cd39
dherv <damien.herv@gmail.com>
2019-06-12 20:46:20 +0900

    feature(words) - Add module

--- src/words/words.module.ts
+++ src/words/words.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { WordsService } from './words.service';
+import { WordsController } from './words.controller';
+import { Word } from './words.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Word])],
+  providers: [WordsService],
+  controllers: [WordsController],
+})
 export class WordsModule {}
