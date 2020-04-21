f4b24ef01deebaae5df5e182030068798b22e2ee
dherv <damien.herv@gmail.com>
2020-04-22 08:38:52 +0900

    refactor(translate): remove loop since response array always contain just one element

--- src/translate/translate.controller.ts
+++ src/translate/translate.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Body, Post } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
 import { TranslationServiceClient } from '@google-cloud/translate';
 
 @Controller('translate')
@@ -10,6 +10,7 @@ export class TranslateController {
       keyFilename: './google_credentials.json',
     });
     const { text, sourceLanguageCode, targetLanguageCode } = body;
+
     // Construct request
     const request = {
       parent: `projects/nikki-1578793478545`,
@@ -22,9 +23,8 @@ export class TranslateController {
     try {
       // Run request
       const [response] = await translationClient.translateText(request);
-      for (const translation of response.translations) {
-        return Promise.resolve({ translation: translation.translatedText });
-      }
+      const translation = response.translations[0].translatedText;
+      return Promise.resolve({ translation });
     } catch (error) {
       return Promise.reject(error);
     }
